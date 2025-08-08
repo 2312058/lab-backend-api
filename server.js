3b0329a796469056c1c20174422061d5455f7c29
2312058 <2312058@nec.edu.in>
2025-08-08 15:16:53 +0530

    Added server.js, models, and routes for CRUD API

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('YOUR_ATLAS_CONNECTION_STRING', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect('mongodb+srv://2312058:joycy1010@cluster0.jbjhed6.mongodb.net/', {
+useNewUrlParser: true,
+useUnifiedTopology: true
 })
 .then(() => console.log("MongoDB Atlas Connected"))
 .catch(err => console.log(err));
@@ -18,7 +18,8 @@ const itemRoutes = require('./routes/itemRoutes');
 app.use('/api/items', itemRoutes);
 
 app.listen(5000, () => console.log("Server running on port 5000"));
-mongoose.connect('mongodb+srv://2312058:joycy1010@cluster0.jbjhed6.mongodb.net/', {
-useNewUrlParser: true,
-useUnifiedTopology: true
-})
+
+
+
+
+
